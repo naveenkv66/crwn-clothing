b2e2db8d8faef17b0a6c8ad1388974f3c0e67b4f
Naveenkumar Venu <naveenkumar.v@falainacloud.com>
2022-12-04 13:18:23 +0800

    Addded Directory and Category Itme component and respective scss

--- src/App.js
+++ src/App.js
@@ -1,53 +1,43 @@
-import './App.css';
+import Directory from './components/directory/directory.component';
 
 const App = () => 
 {
 
   const categories = [
     {
-      id: 1,
-      title: 'Hats',
-
+      "id": 1,
+      "title": "hats",
+      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
     },
     {
-      id: 2,
-      title: 'Sneakers',
-
+      "id": 2,
+      "title": "jackets",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
     },
     {
-      id: 3,
-      title: 'Jackets',
-
+      "id": 3,
+      "title": "sneakers",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
     },
     {
-      id: 4,
-      title: 'Men',
-
+      "id": 4,
+      "title": "womens",
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
     },
     {
-      id: 5,
-      title: 'Women',
-
+      "id": 5,
+      "title": "mens",
+      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
     }
   ]
   return (
-    <div className="categories-container">
-      {
-        categories.map(({ title, id }) =>
-        {
+    <div>
 
-          return (<div key={id} className="categorie-container">
-            <img className="background-image"></img>
-            <div className="categorie-body-container">
-              <h1 >{title}</h1>
-              <h2 >Shop Now</h2>
-            </div>
-          </div>)
 
-        })
-      }
+      <Directory categories={categories} ></Directory>
 
     </div>
+
   )
 }
 
